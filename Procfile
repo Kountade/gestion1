b20680b3db882beafc4f7276f4b2f3caccb94707
Bounama Kounta <bounamakountagoudiaby@gmail.com>
2024-07-06 14:55:30 +0100

    first commit 1

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn gestion1.wsgi
+
--- gestion1/settings.py
+++ gestion1/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -133,6 +134,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"]
 
 STATIC_ROOT = (BASE_DIR/"asert/")
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 
 # Default primary key field type
